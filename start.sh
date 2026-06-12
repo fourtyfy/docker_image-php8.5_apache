cfc0a15243665f8a16c9184730a6becbf0853889
fourtyfy <thomas.lindner11@gmail.com>
2026-06-12 15:47:51 +0200

    Update start.sh to modify user/group and run composer

--- start.sh
+++ start.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 set -e
 
+# Default to 1000 if not provided
 UID=${UID:-1000}
 GID=${GID:-1000}
 
+echo "Starting with UID=$UID GID=$GID"
+
+# Only modify user/group if needed
 CURRENT_UID=$(id -u www-data)
 CURRENT_GID=$(id -g www-data)
 
 if [ "$CURRENT_GID" != "$GID" ]; then
+    echo "Updating group www-data to GID $GID"
     groupmod -o -g "$GID" www-data
 fi
 
 if [ "$CURRENT_UID" != "$UID" ]; then
+    echo "Updating user www-data to UID $UID"
     usermod -o -u "$UID" www-data
 fi
 
+# Fix permissions inside container
 chown -R www-data:www-data /var/www
 
 cd /var/www/html
 
+# Optional composer step
 if [ -f composer.json ]; then
-    echo "Running composer update..."
-    composer update
+    echo "Running composer install/update..."
+    composer install || composer update
 fi
 
+# Start Apache in foreground
 exec apache2-foreground
